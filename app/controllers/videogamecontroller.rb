21b389265ed44cd8f8fbfb3ce082644e21b4ae64
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-10-12 23:53:46 +0000

    added index

--- app/controllers/videogamecontroller.rb
+++ app/controllers/videogamecontroller.rb
@@ -2,14 +2,27 @@ class VideogamesController < ApplicationController
   get '/videogames'
     erb :index
   end
-
+  
+  get '/videogames/new' do 
+    erb :'videogames/new'
+  end 
 
   get '/videogames/:id' do
     @videogame = Videogame.find(id: params[:id])
 
     if @videogame
-      redirect :"/videogames/#{videogame.id}"
+      redirect :"/videogames/show"
     else
       redirect '/videogames'
   end
+  
+  post '/videogames' do 
+    @videogame = Videogame.new(name: params[:name],console: params[:console],genre: params[:genre])
+    
+    if @videogame.save
+      redirect "/videogames/#{@videogame.id}"
+    else 
+      redirect '/videogames/new'
+    end 
+  end 
 end
