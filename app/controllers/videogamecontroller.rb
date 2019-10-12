0169f9d9a11885a0b683412d0c6bac25acc81a8f
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-10-12 22:25:30 +0000

    added tables

--- app/controllers/videogamecontroller.rb
+++ app/controllers/videogamecontroller.rb
@@ -1,3 +1,15 @@
 class VideogamesController < ApplicationController
-  
+  get '/videogames'
+    erb :index
+  end
+
+
+  get '/videogames/:id' do
+    @videogame = Videogame.find(id: params[:id])
+
+    if @videogame
+      redirect :"/videogames/#{videogame.id}"
+    else
+      redirect '/videogames'
+  end
 end
--- db/migrate/20191012221518_create_users.rb
+++ db/migrate/20191012221518_create_users.rb
@@ -1,4 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+    end
+  end
+  def up
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+    end
+  end
+
+  def down
+    drop_table :users
   end
 end
