a8f1577b9e07218b2d295e5f8acabbf712da071e
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-10-12 22:13:34 +0000

    fixed laction problems

--- app/controllers/videogamecontroller.rb
+++ app/controllers/videogamecontroller.rb
@@ -1,17 +1,3 @@
 class VideogamesController < ApplicationController
 
-  get '/videogames'
-    erb :index
-  end
-
-
-  get '/videogames/:id' do
-    @videogame = Videogame.find(id: params[:id])
-
-    if @videogame
-      redirect :"/videogames/#{videogame.id}"
-    else
-      redirect '/videogames'
-    end
-  end
 end
